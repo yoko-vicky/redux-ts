3bd7e670f13c801ed117e202d741df3587c7e3c9
yocosaka <sakayoco55@gmail.com>
2022-02-26 18:13:09 +0900

    Get all fetched data using useSelector method of react-redux

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchReporistories } = useActions();
+  const { data, error, loading } = useSelector(
+    (state: any) => state.repositories,
+  );
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
